ff03f4e314d506bfe0682adb497bc8b2177a7bd7
albin29 <albin.duraku00@gmail.com>
2024-01-16 21:30:07 +0100

    v2

--- Models.cs
+++ Models.cs
@@ -5,36 +5,30 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 public class User
 {
-    [Key]
-    public int Id { get; set; }
+    public int? Id { get; set; }
     public string? Username { get; set; }
     public string? Password { get; set; }
-    public int? PostId { get; set; }
-    public Post Post { get; set; }
+    public List<int> Posts { get; set; }  
 }
 public class Post
 {
-    [Key]
     public int? Id { get; set; }
     public string? Title { get; set; }
     public string? Content { get; set; }
     public string? PublishedOn { get; set; }
-    public int? BlogId { get; set; }
-    [ForeignKey("BlogId")]
     public Blog Blog { get; set; }
-    public int? UserId { get; set; }
-    [ForeignKey("UserId")]
     public User User { get; set; }
+    public int? BlogId { get; set; }
+    public int? UserId { get; set; }
+   
 }
 public class Blog
 {
-    [Key]
     public int? Id { get; set; }
-    public int BlogId { get; set; }
     public string? Url { get; set; }
     public string? Name { get; set; }
-    public int? PostId { get; set; }
-    public Post Post { get; set; }
+    public List<int> Posts { get; set; }
+
 }
 public class BloggingContext : DbContext
 {
--- Program.cs
+++ Program.cs
@@ -8,19 +8,78 @@ class Program
     {
         Console.WriteLine("Hello");
         string[] linesBlogs = File.ReadAllLines("../../../Blogs.csv");
+
         string[] linesUsers = File.ReadAllLines("../../../Users.csv");
         string[] linesPosts = File.ReadAllLines("../../../Posts.csv");
         using (var db = new BloggingContext())
         {
+            List<int> Blogid1 = new List<int>();
+            List<int> Blogid2 = new List<int>();
+
             foreach (string line in linesBlogs)
             {
                 string[] blogLine = line.Split(',');
-                int blogId = int.Parse(blogLine[0]);
+                int Posts = int.Parse(blogLine[3]);
+                int blogid = int.Parse(blogLine[0]);
+                if (blogid == 1)
+                {
+                    Blogid1.Add(Posts);
+                }
+                if (blogid == 2)
+                {
+                    Blogid2.Add(Posts);
+                }
+            }
+
+            string test = string.Empty;
+            int count = 0;
+            foreach (string line in linesBlogs)
+            {
+                string[] blogLine = line.Split(',');
                 string url = blogLine[1];
                 string name = blogLine[2];
                 int postid = int.Parse(blogLine[3]);
-                db.Blogs.Add(new Blog { BlogId = blogId, Url = url, Name = name, PostId = postid });
+
+                if (test != url)
+                {
+
+                    if (count == 0)
+                    {
+
+                        db.Blogs.Add(new Blog { Url = url, Name = name, Posts = Blogid1 });
+                        count++;
+                    }
+
+                    else
+                    {
+                        db.Blogs.Add(new Blog { Url = url, Name = name, Posts = Blogid2 });
+                        break;
+
+                    }
+                }
+                test = url;
             }
+
+            List<int> Userid1 = new List<int>();
+            List<int> Userid2 = new List<int>();
+
+            foreach (string line in linesUsers)
+            {
+                string[] userLine = line.Split(',');
+                int Posts = int.Parse(userLine[3]);
+                int userid = int.Parse(userLine[0]);
+                if (userid == 1)
+                {
+                    Userid1.Add(Posts);
+                }
+                if (userid == 2)
+                {
+                    Userid2.Add(Posts);
+                }
+
+            }
+            string test1 = string.Empty;
+            int count1 = 0;
             foreach (string line in linesUsers)
             {
                 string[] userLine = line.Split(',');
@@ -28,7 +87,20 @@ class Program
                 string username = userLine[1];
                 string password = userLine[2];
                 int postId = int.Parse(userLine[3]);
-                db.Users.Add(new User { Id = userId, Username = username, Password = password, PostId = postId });
+                if (test1 != username)
+                {
+                    if (count1 == 0)
+                    {
+                        db.Users.Add(new User { Id = userId, Username = username, Password = password, Posts = Userid1 });
+                        count1++;
+                    }
+                    else
+                    {
+                        db.Users.Add(new User { Id = userId, Username = username, Password = password, Posts = Userid2 });
+                        break;
+                    }
+                }
+                test1 = username;
             }
             foreach (string line in linesPosts)
             {
@@ -49,11 +121,13 @@ class Program
                 Console.WriteLine($"BlogId: {blog.Id}, Url: {blog.Url}, Name: {blog.Name}");
             }
             var users = db.Users.ToList();
+
             Console.WriteLine("\nUsers:");
             foreach (var user in users)
             {
-                Console.WriteLine($"UserId: {user.Id}, Username: {user.Username}, Password: {user.Password}, PostId: {user.PostId}");
+                Console.WriteLine($"UserId: {user.Id}, Username: {user.Username}, Password: {user.Password}");
             }
+
             var posts = db.Posts.ToList();
             Console.WriteLine("\nPosts:");
             foreach (var post in posts)
